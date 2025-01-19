225e3eff2431918d6fd0f3d7bd1f2cc9411198de
BugHunter-X <askzenai@gmail.com>
2025-01-19 19:52:06 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,6 @@
+function foo(a,b){return a+b;}
+console.log(foo(2,3)); //5
+
+function bar(a,b){return a-b;}
+console.log(bar(2,3)); //-1
+//This solution uses a new function name (bar) to solve the redefinition problem.
